ce7ebe576d0176a2e50dc221f3bd36b6a6706fee
Jeff Gentry <geoffjentry@gmail.com>
2011-04-22 02:42:55 +0000

    prevent rate throttling

--- R/comm.R
+++ R/comm.R
@@ -13,12 +13,42 @@ getAPIKey <- function() {
   get('key', envir=apiKey)
 }
 
+
+## The apiCallQueue class is just a way to prevent rate throttling.  We can't send more than
+## 30 requests in a 5 second period.
+setRefClass('apiCallQueue',
+            fields = list(
+              calls = 'list'
+              ),
+            methods = list(
+              pushTime = function() {
+                .self$calls <- c(Sys.time(), .self$calls)[1:29]
+              },
+              curDiff = function() {
+                ## returns 7 if the calls list isn't full (29 elements) as
+                ## by definition we don't need to rate limit
+                if (is.na(.self$calls[[29]]))
+                  return(7)
+                as.numeric(.self$calls[[1]] - .self$calls[[29]])
+              }
+              )
+            )
+
+apiCallQueueFactory <- getRefClass('apiCallQueue')
+callQueue <- apiCallQueueFactory$new()
+
 doAPICall <- function(url, ...) {
   ## FIXME:  Will get blocked if more than 30 requests in 5 seconds are made.
   ##   Detect if this happens and automagically limit things.
   key <- try(getAPIKey(), silent=TRUE)
   if (!inherits(key, 'try-error'))
     url <- paste(url, '&key=', key)
+  ## We need to be careful of throttling here, as 30 calls in 5 seconds will get us
+  ## blocked.  Being conservative (queue holds only 29 elements, and we'll diff on 6 seconds)
+  callQueue$pushTime()
+  while (callQueue$curDiff() < 6) {
+    TRUE
+  }
   json <- getURL(url, .opts=list(encoding="identity,gzip"))
   out <- fromJSON(json)
 
